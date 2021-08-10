5a0085eabfaaadb97bef705d4f12941a3738c545
pablo.gonzales.2009 <66168769+pablogonzales2009@users.noreply.github.com>
2021-08-10 11:12:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import random
+from random import choice, shuffle
 
 import time
 
@@ -15,10 +16,10 @@ random_messages = ['example 1', 'example 2', 'example 3'] # put your random mess
 async def on_ready():
     CHANNEL_ID = open("channel.txt","r").readline() # channel id
     channel = client.get_channel(int(CHANNEL_ID))
-    message = random_messages[random.randrange(0, len(random_messages))] # using the random library, get a message
+    message = (random.choice(random_messages)) # using the random library, get a message
     await channel.send(message) 
     while True: # very lazy solution to loop it forever
         time.sleep(100)
-        message = random_messages[random.randrange(0, len(random_messages))] # using the random library, get a message
+        message = (random.choice(random_messages)) # using the random library, get a message # random_messages[random.randrange(0, len(random_messages))]
         await channel.send(message)
 client.run(TOKEN)
