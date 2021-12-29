7740f758c69912087627fb0865d0c1688fe3415c
pablo <66168769+pablogonzales2009@users.noreply.github.com>
2021-12-29 11:14:18 +0100

    wow i was dumb
    s

--- bot.py
+++ bot.py
@@ -19,6 +19,6 @@ async def on_ready():
     await channel.send(message) 
     while True: # very lazy solution to loop it forever 
         time.sleep(100)
-        message = (random.choice(random_messages)) # alternative: random_messages[random.randrange(0, len(random_messages))]
+        message = (random.choice(random_messages))
         await channel.send(message)
 client.run(TOKEN)
