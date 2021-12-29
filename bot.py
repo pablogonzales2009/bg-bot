87ef368474b861ec1efe3afe99f0a0c48ac66b3e
pablo <66168769+pablogonzales2009@users.noreply.github.com>
2021-12-29 11:14:05 +0100

    wow i was dumb

--- bot.py
+++ bot.py
@@ -7,19 +7,18 @@ import discord
 
 import os
 
-TOKEN = open("TOKEN.txt","r").readline() # token.txt just includes the bot token
+TOKEN = inserttokenhere
 
 client = discord.Client()
 random_messages = ['example 1', 'example 2', 'example 3'] # put your random messages here
 
 @client.event
 async def on_ready():
-    CHANNEL_ID = open("channel.txt","r").readline() # channel id
-    channel = client.get_channel(int(CHANNEL_ID))
-    message = (random.choice(random_messages)) # using the random library, get a message
+    channel = client.get_channel(#insert ur channel id here)
+    message = (random.choice(random_messages)) # alternative: random_messages[random.randrange(0, len(random_messages))]
     await channel.send(message) 
     while True: # very lazy solution to loop it forever 
         time.sleep(100)
-        message = (random.choice(random_messages)) # using the random library, get a message # alternative: random_messages[random.randrange(0, len(random_messages))]
+        message = (random.choice(random_messages)) # alternative: random_messages[random.randrange(0, len(random_messages))]
         await channel.send(message)
 client.run(TOKEN)
